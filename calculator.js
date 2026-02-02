22ff661af8703657f039cd67f2272ae292a6f88f
Abdi3214 <raboy454@gmail.com>
2026-02-02 17:32:19 +0300

    Create calculator with add, subtract, multiply, and divide operations

---
+++ calculator.js
@@ -0,0 +1,79 @@
+const btn = document.querySelectorAll("button");
+const display = document.querySelector("#display");
+
+let fistNumber = "";
+let operator = "";
+let secondNumber = "";
+
+const Add = (fistNumber, secondNumber) => {
+  const result = fistNumber + secondNumber;
+  return result;
+};
+
+const Substract = (fistNumber, secondNumber) => {
+  const result = fistNumber - secondNumber;
+  return result;
+};
+
+const Multiply = (fistNumber, secondNumber) => {
+  const result = fistNumber * secondNumber;
+  return result;
+};
+
+const Divide = (fistNumber, secondNumber) => {
+  const result = fistNumber / secondNumber;
+  return result;
+};
+
+const operate = (fistNumber, operator, secondNumber) => {
+  if (operator === "+") {
+    return Add(fistNumber, secondNumber);
+  }
+  if (operator === "-") {
+    return Substract(fistNumber, secondNumber);
+  }
+  if (operator === "*") {
+    return Multiply(fistNumber, secondNumber);
+  }
+  if (operator === "/") {
+    if (secondNumber === 0) {
+      alert("Can't divide by zero");
+      return;
+    }
+    return Divide(fistNumber, secondNumber);
+  }
+};
+
+btn.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+    if (value === "Clear") {
+      fistNumber = "";
+      operator = "";
+      secondNumber = "";
+      display.textContent = "";
+    }
+    if (!isNaN(value) && operator === "") {
+      fistNumber = fistNumber * 10 + Number(value);
+      display.textContent = fistNumber;
+    }
+    if (
+      fistNumber !== "" &&
+      (value === "+" || value === "-" || value === "*" || value === "/")
+    ) {
+      operator = value;
+      display.textContent = fistNumber + operator;
+    }
+    if (!isNaN(value) && operator !== "") {
+      secondNumber = secondNumber * 10 + Number(value);
+      display.textContent = fistNumber + operator + secondNumber;
+    }
+    if (value === "=") {
+      const result = operate(fistNumber, operator, secondNumber);
+      display.textContent = result;
+      fistNumber = result;
+      operator = 0;
+      secondNumber = 0;
+    }
+  });
+});
